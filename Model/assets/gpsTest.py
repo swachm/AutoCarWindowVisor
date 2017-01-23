ffcf6e5c4770e9833b59628ae435e97821d19abd
manmeetswach <Shriyantra#1>
2017-01-23 13:16:43 -0800

    Added the actural GPS data extrator file to the project.

--- Model/assets/gpsTest.py
+++ Model/assets/gpsTest.py
@@ -3,7 +3,8 @@ import gps
 # Listen on port 2947 (gpsd) of localhost
 session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
- 
+
+
 while True:
     try:
     	report = session.next()
@@ -12,11 +13,12 @@ while True:
 		# print report
         if report['class'] == 'TPV':
             if hasattr(report, 'time'):
-                print report.time
+                print(report.time)
     except KeyError:
 		pass
+
     except KeyboardInterrupt:
 		quit()
     except StopIteration:
 		session = None
-		print "GPSD has terminated"
+		print("GPSD has terminated")
